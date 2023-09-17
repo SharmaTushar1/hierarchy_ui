b9904fa100dc4690216527ee3cb3e6c63a3312ee
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-17 14:26:16 +0530

    Add positions

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,9 +3,11 @@ import { create } from 'zustand';
 interface Type {
   rootEmployee: CEO; // the root employee will be the CEO and we will use that to design the hierarchy
   setRootEmployee: (newValue: CEO) => void;
+  positions: {[key: number]: string};
 }
 
 export const useStore = create<Type>((set) => ({
   rootEmployee: JSON.parse(localStorage.getItem('rootEmployee') as string) || {},
   setRootEmployee: (newValue: CEO) => set({rootEmployee: newValue}),
+  positions: {0:'CEO', 1:'Head of Design', 2:'Head of Staff', 3:'Head of Engineering', 4:'Team Leader', 5:'Team Member'}
 }))
