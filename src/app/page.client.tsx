6e81c10a6d18f841f0c24d1b3180842d59161bcb
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-17 09:59:32 +0530

    Add button

--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -1,6 +1,24 @@
+'use client';
+
+import { useState } from "react";
+import { useStore } from "./store";
+
+
 const PageClient = () => {
+
+  const rootEmployee = useStore(store => store.rootEmployee);
+  const [addMemberPopUp, setAddMemberPopUp] = useState(false);
+
+  const toggleAddMemberPopUp = () => {
+    setAddMemberPopUp(prev => !prev);
+  }
+
   return (
-    <div>PageClient</div>
+    <div>
+      {Object.values(rootEmployee).length==0 ? (<button className="m-8" onClick={() => toggleAddMemberPopUp()}>Add Member</button>): (
+        'The root employee is present'
+      )}
+    </div>
   )
 }
 export default PageClient
