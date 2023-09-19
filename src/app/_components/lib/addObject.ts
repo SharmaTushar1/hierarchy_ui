396d199632a1007faf5550fa9058daae8d36a43b
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-19 13:56:06 +0530

    Remove log statements

--- src/app/_components/lib/addObject.ts
+++ src/app/_components/lib/addObject.ts
@@ -1,14 +1,9 @@
 // TODO: Use this function for adding object everywhere
 
 export const addObject = (root: CEO | Head | Team | Member, id: string, objectToAdd: Head | Team | Member) => {
-  console.log("root from addObject => ", root);
   //@ts-ignore
   return root.items.map((obj) => {
-    console.log("obj.id => ",obj.id, ", id => ",id);
     if (obj.id == id) {
-      console.log("obj => ", obj)
-      console.log("obj.items => ",obj.items);
-      console.log("objectToAdd => ", objectToAdd);
       return {
         ...obj,
         items: [ //@ts-ignore
