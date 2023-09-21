4b90f8175ca7e514839d8e8f85e30682b4d106ab
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-21 11:33:28 +0530

    Improve UI for filter

--- src/app/filter/_components/ShowItems.tsx
+++ src/app/filter/_components/ShowItems.tsx
@@ -2,12 +2,22 @@ const ShowItems = ({array}: {array: Employee[]}) => {
   return (
     <div>
       {array.map((item: Employee) => (
-        <div key={item.id}>
-          ID: {item.id}
-          Name: {item.name}
-          Phone Number: {item.phone_number}
-          Email: {item.email}
-          position: {item.position}
+        <div className="border-8 border-red-800 m-4" key={item.id}>
+          <div>
+            ID: {item.id}
+          </div>
+          <div>
+            Name: {item.name}
+          </div>
+          <div>
+            Phone Number: {item.phone_number}
+          </div>
+          <div>
+            Email: {item.email}
+          </div>
+          <div>
+            position: {item.position}
+          </div>
         </div>
       ))}
     </div>
