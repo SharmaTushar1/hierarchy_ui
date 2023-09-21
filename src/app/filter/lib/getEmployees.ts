4fab0015c4ff0065989fd0df080c175459d6425d
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-21 19:35:42 +0530

    Add edge case handling

--- src/app/filter/lib/getEmployees.ts
+++ src/app/filter/lib/getEmployees.ts
@@ -1,17 +1,22 @@
+// A function to get all the employees. So everything except the teams.
+
 import { isTeam } from "@/app/_components/lib/typeChecker";
 
-export const getEmployees = (root: any, allEmployees: Employee[]) => {
+export const getEmployees = (root: CEO | Head | Team, allEmployees: Employee[]): void => {
+  if (!root) {
+    return;
+  }
   if (!isTeam(root)) {
-    let newObj = {id: root.id, name: root.name, phone_number: root.phone_number, email: root.email, position: undefined};
+    let newObj = {id: root.id, name: root.name, phone_number: root.phone_number, email: root.email, position: undefined}; // @ts-ignore
     newObj = root.position?{...newObj, position: root.position}:root.isLeader?{...newObj, position: 'Team Leader'}:{...newObj, position: 'Team Member'};
     allEmployees.push(newObj);
   }
-  //@ts-ignore the reason I'm mapping here despite worrying about if there's an items or not in the root is because the root in the beginning will always be CEO
-  return root.items.map((obj) => {
-    if (obj.items) { // not a member or the last of the chain
-      return getEmployees(obj, allEmployees);
-    } else {
-      let newObj = {id: obj.id, name: obj.name, phone_number: obj.phone_number, email: obj.email, position: undefined};
+  if (!root.items) return;
+  root.items.map((obj: Head | Team | Member) => { // @ts-ignore
+    if (obj.items) { // @ts-ignore not a member or the last of the chain
+      getEmployees(obj, allEmployees);
+    } else { // @ts-ignore
+      let newObj = {id: obj.id, name: obj.name, phone_number: obj.phone_number, email: obj.email, position: undefined}; // @ts-ignore
       newObj = obj.position?{...newObj, position: obj.position}:obj.isLeader?{...newObj, position: 'Team Leader'}:{...newObj, position: 'Team Member'};
       allEmployees.push(newObj);
     }
