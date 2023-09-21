c429f9c25fa05cd8ee6f7ae60f2c86b8094e90a6
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-21 17:37:06 +0530

    Remove warnings

--- src/app/lib/findParent.ts
+++ src/app/lib/findParent.ts
@@ -2,8 +2,8 @@ export const findParent = (root: CEO | Head | Team, childId: string): CEO | Head
   for (let obj of root.items) {
     if (obj.id === childId) {
       return root;
-    }
-    if (!obj.items) continue;
+    } // @ts-ignore obj.items basically means a check if it's a member or not while checking if there are more items in it.
+    if (!obj.items) continue; // @ts-ignore it won't be item
     let foundTeam = findParent(obj, childId);
     if (foundTeam) return foundTeam;
   }
--- src/app/lib/findTeam.ts
+++ src/app/lib/findTeam.ts
@@ -1,7 +1,7 @@
 import { isTeam } from "../_components/lib/typeChecker";
 
 export const findTeam = (root: CEO | Head | Team, id: string): Team | undefined => {
-  if (root.id === id) {// here the id we will pass will always be of the team so root will be team and hence we can assign it while ignoring the warning
+  if (root.id === id) {// @ts-ignore here the id we will pass will always be of the team so root returned will always be a team and hence we can assign it while ignoring the warning
     return {...root};
   }
   if (isTeam(root)) return undefined; // if the id is not same as what we are looking for and it's still a team then we can return and look for the next teams under the head.
